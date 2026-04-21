3b1ba464a5e4c4c44f23b0348a109fdef8119770
Louie D <louie.delrosario97@gmail.com>
2026-04-21 15:13:04 -0700

    Initial commit. Started Employee class. Created data members, constructors, getters, and setters

---
+++ src/com/pluralsight/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    public static void main(String[] args) {
+        private int employeeId;   // private data members
+        private String name;
+        private int hoursWorked;
+        private double payRate;
+
+    }
+}
+
