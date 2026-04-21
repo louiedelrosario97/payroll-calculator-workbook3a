3b2fd9a04c2b104e4d376b2f11594cde3867ae95
Louie D <louie.delrosario97@gmail.com>
2026-04-21 15:37:08 -0700

    created variable employeeData to represent our employee-data.csv file. imported FileReader and BufferedReader

--- src/com/pluralsight/Employee.java
+++ src/com/pluralsight/Employee.java
@@ -1,10 +1,38 @@
+package com.pluralsight;
+
 public class Employee {
-    public static void main(String[] args) {
         private int employeeId;   // private data members
         private String name;
-        private int hoursWorked;
+        private double hoursWorked;
         private double payRate;
 
+        // Parameterized constructor.
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
     }
+ // getters and setters
+    public int getEmployeeId() { return employeeId; }
+
+    public void setEmployeeId(int employeeId) { this.employeeId = employeeId; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public double getHoursWorked() { return hoursWorked; }
+
+    public void setHoursWorked(double hoursWorked) { this.hoursWorked = hoursWorked; }
+
+    public double getPayRate() { return payRate; }
+
+    public void setPayRate(double payRate) { this.payRate = payRate; }
+
+
+    // getGrossPay(), this calculates and returns that employee's gross pay based on their hours worked and pay rate.
+    double grossPay() { return hoursWorked * payRate; }
 }
+
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,18 @@
-package main;public class Main {
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
     public static void main(String[] args) {
-        
+
+        String employeeData = "employee-data.csv";
+        try {
+            FileReader fileReader = new FileReader();
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String input = bufferedReader.readLine();
+        }
+
     }
 }
 
