a5193970b9be56263e246cb2267a18b23fdaed39
Louie D <louie.delrosario97@gmail.com>
2026-04-21 21:51:08 -0700

    set up the bufferedReader

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,17 +2,22 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
         String employeeData = "employee-data.csv";
         try {
-            FileReader fileReader = new FileReader();
+            FileReader fileReader = new FileReader(employeeData);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String input = bufferedReader.readLine();
-        }
 
+            System.out.println(bufferedReader.readLine()); // We can read the file with this line, now we need to make a while loop to read the info we want
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
 
